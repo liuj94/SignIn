e0a8039a58047e9193173c3dfc7cbcee38d8a079
liujie <871933919@qq.com>
2023-02-22 06:45:10 +0800

    4

--- app/src/main/java/com/example/signin/PageRoutes.kt
+++ app/src/main/java/com/example/signin/PageRoutes.kt
@@ -13,6 +13,12 @@ interface PageRoutes {
         val Api_getUserInfo = "$BaseUrl/getInfo"
         //appStatus	-1,全部;1,进行中;2,过期
         val Api_meetingList = "$BaseUrl/manager/meeting/list"
+        val Api_meeting_statistics = "$BaseUrl/manager/meeting/meeting/statistics/{id}"
+        val Api_meeting_business = "$BaseUrl/manager/meeting/business/{id}"
+
+        //type 1 注册签到2 来程签到3 入住签到4 会场签到5 餐饮签到6 礼品签到7 返程签到
+        val Api_meetingSignUpLocationList = "$BaseUrl/manager/meetingSignUpLocation/list"
+        val Api_meetingSignUpLocation = "$BaseUrl/manager/meetingSignUpLocation/{id}"
 
     }
 
