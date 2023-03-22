3dbbf0669953644f6bf8e7e83c6c5c49638b2dde
liuj <liuj@zcitc.com>
2023-03-22 15:40:54 +0800

    aaa

--- app/src/main/java/com/example/signin/SpeechUtils.java
+++ app/src/main/java/com/example/signin/SpeechUtils.java
@@ -35,7 +35,7 @@ public class SpeechUtils {
                 if (i == TextToSpeech.SUCCESS) {
                     //textToSpeech.setLanguage(Locale.US);
                     textToSpeech.setLanguage(Locale.CHINA);
-                    textToSpeech.setPitch(1.4f);// 设置音调，值越大声音越尖（女生），值越小则变成男声,1.0是常规
+                    textToSpeech.setPitch(1.0f);// 设置音调，值越大声音越尖（女生），值越小则变成男声,1.0是常规
                     textToSpeech.setSpeechRate(0.7f);
 
                 }
